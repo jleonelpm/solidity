6ea97cff83026f3821c654a86f0afad6a5c713b8
Leonel Pech <jleonelpm@hotmail.com>
2021-08-03 13:48:24 -0500

    Se aplicó modificador payable

--- MessageStore.sol
+++ MessageStore.sol
@@ -15,9 +15,15 @@ contract MessageStore {
     }
     
     //Método público que recibe un argumento de tipo String
-    function setMessage(string newMessage) public isOwner { //Se aplicó el modificador
+    function setMessage1(string newMessage) public isOwner { //Se aplicó elmodificador
         message = newMessage;
     }
+
+    //Método público que recibe un argumento de tipo String
+    function setMessage2(string newMessage) public payable { //Se aplicó payable
+        require(msg.value == 3 ether); // se deberá ejecutar solamente el valor pagado en ether es 3
+        message = newMessage;
+    }    
     
     //Método público que retorna un mesaje de tipo string
     function getMessage() public view returns (string) {
